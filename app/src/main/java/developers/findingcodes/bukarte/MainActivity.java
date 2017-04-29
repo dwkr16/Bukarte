c4d87431086561990563e65394b81b662f7e09a1
Diwakar Kumar <dwkr16@gmail.com>
2017-04-29 22:04:54 +0530

    Initial commit

---
+++ app/src/main/java/developers/findingcodes/bukarte/MainActivity.java
@@ -0,0 +1,13 @@
+package developers.findingcodes.bukarte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
