1e0c11c13d90239053d9a7c527329ae8b9b56534
Diwakar Kumar <dwkr16@gmail.com>
2017-04-30 11:20:04 +0530

    1st Commint

--- app/src/main/java/developers/findingcodes/bukarte/MainActivity.java
+++ app/src/main/java/developers/findingcodes/bukarte/MainActivity.java
@@ -1,13 +1,106 @@
 package developers.findingcodes.bukarte;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String REGISTER_URL = "http://developers.bukarte.com/example_api/usersignup";
+
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "c_password";
+    public static final String KEY_USERNAME = "c_name";
+
+
+    private EditText editTextUsername;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextEmail= (EditText) findViewById(R.id.editTextEmail);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+
+        Map<String, String> postParam= new HashMap<String, String>();
+        postParam.put(KEY_EMAIL,email);
+        postParam.put(KEY_PASSWORD, password);
+        postParam.put(KEY_USERNAME, username);
+
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                REGISTER_URL, new JSONObject(postParam),
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(MainActivity.this, (CharSequence) response,Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+                    public void onErrorResponse(VolleyError error) {
+                       Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                   }
+        }) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+
+
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjReq);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            registerUser();
+
+        }
     }
 }
